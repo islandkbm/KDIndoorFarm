19374006d96c026f849438e9cb5568fb116c66e9
islandkbm <islandkbm@gmail.com>
2021-06-02 16:03:23 +0900

    up2

--- frontend/farmapp/src/pages/sensorpage.js
+++ frontend/farmapp/src/pages/sensorpage.js
@@ -1,72 +1,169 @@
-import React, { Component,useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Sensordisplay from "../sensordisplay";
 import IndoorFarmAPI from "../indoorfarmapi";
-import { ResponsiveLine } from '@nivo/line'
+import { ResponsiveBump } from '@nivo/bump'
 
 
-const MyResponsiveLine = ({ data /* see data tab */ }) => (
-  <ResponsiveLine
+
+// make sure parent container have a defined height when using
+// responsive component, otherwise height will be 0 and
+// no chart will be rendered.
+// website examples showcase many properties,
+// you'll often use just a few of them.
+function MyResponsiveBump( data)
+{
+
+return (
+  <ResponsiveBump
       data={data}
-      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-      xScale={{ type: 'point' }}
-      yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
-      yFormat=" >-.2f"
-      axisTop={null}
+      margin={{ top: 40, right: 100, bottom: 40, left: 60 }}
+      colors={{ scheme: 'spectral' }}
+      lineWidth={3}
+      activeLineWidth={6}
+      inactiveLineWidth={3}
+      inactiveOpacity={0.15}
+      pointSize={10}
+      activePointSize={16}
+      inactivePointSize={0}
+      pointColor={{ theme: 'background' }}
+      pointBorderWidth={3}
+      activePointBorderWidth={3}
+      pointBorderColor={{ from: 'serie.color' }}
+      axisTop={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: '',
+          legendPosition: 'middle',
+          legendOffset: -36
+      }}
       axisRight={null}
       axisBottom={{
-          orient: 'bottom',
           tickSize: 5,
           tickPadding: 5,
           tickRotation: 0,
-          legend: 'transportation',
-          legendOffset: 36,
-          legendPosition: 'middle'
+          legend: '',
+          legendPosition: 'middle',
+          legendOffset: 32
       }}
       axisLeft={{
-          orient: 'left',
           tickSize: 5,
           tickPadding: 5,
           tickRotation: 0,
-          legend: 'count',
-          legendOffset: -40,
-          legendPosition: 'middle'
+          legend: 'ranking',
+          legendPosition: 'middle',
+          legendOffset: -40
       }}
-      pointSize={10}
-      pointColor={{ theme: 'background' }}
-      pointBorderWidth={2}
-      pointBorderColor={{ from: 'serieColor' }}
-      pointLabelYOffset={-12}
-      useMesh={true}
-      legends={[
-          {
-              anchor: 'bottom-right',
-              direction: 'column',
-              justify: false,
-              translateX: 100,
-              translateY: 0,
-              itemsSpacing: 0,
-              itemDirection: 'left-to-right',
-              itemWidth: 80,
-              itemHeight: 20,
-              itemOpacity: 0.75,
-              symbolSize: 12,
-              symbolShape: 'circle',
-              symbolBorderColor: 'rgba(0, 0, 0, .5)',
-              effects: [
-                  {
-                      on: 'hover',
-                      style: {
-                          itemBackground: 'rgba(0, 0, 0, .03)',
-                          itemOpacity: 1
-                      }
-                  }
-              ]
-          }
-      ]}
   />
-)
+);
+    }
+
 
 const Sensorpage = () => {
+
+  const h1data = [
+    {
+      "id": "First",
+      "data": [
+        {
+          "x": 2000,
+          "y": 4
+        },
+        {
+          "x": 2001,
+          "y": 7
+        },
+        {
+          "x": 2002,
+          "y": 11
+        },
+        {
+          "x": 2003,
+          "y": 12
+        },
+        {
+          "x": 2004,
+          "y": 2
+        }
+      ]
+    },
+    {
+      "id": "Second",
+      "data": [
+        {
+          "x": 2000,
+          "y": 10
+        },
+        {
+          "x": 2001,
+          "y": 2
+        },
+        {
+          "x": 2002,
+          "y": 3
+        },
+        {
+          "x": 2003,
+          "y": 7
+        },
+        {
+          "x": 2004,
+          "y": 8
+        }
+      ]
+    },
+    {
+      "id": "Last",
+      "data": [
+        {
+          "x": 2000,
+          "y": 9
+        },
+        {
+          "x": 2001,
+          "y": 5
+        },
+        {
+          "x": 2002,
+          "y": 8
+        },
+        {
+          "x": 2003,
+          "y": 5
+        },
+        {
+          "x": 2004,
+          "y": 12
+        }
+      ]
+    },
+    {
+      "id": "Serie 4",
+      "data": [
+        {
+          "x": 2000,
+          "y": 7
+        },
+        {
+          "x": 2001,
+          "y": 4
+        },
+        {
+          "x": 2002,
+          "y": 2
+        },
+        {
+          "x": 2003,
+          "y": 11
+        },
+        {
+          "x": 2004,
+          "y": 9
+        }
+      ]
+    }
+  
+  ];
 
 
     const [msensorsarray, setSensors] = useState([]);
@@ -89,7 +186,7 @@ const Sensorpage = () => {
             <div className="sensorbocck">
               {Sensordisplay(msensorsarray,true)}
 
-              {MyResponsiveLine(null)}
+
 
               </div>
         
